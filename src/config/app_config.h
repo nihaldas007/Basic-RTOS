da964e5eb347328e9e9a8fa0661193793eda4846
nihaldas007 <nihaldas007@gmail.com>
2025-11-25 23:25:00 +0600

    update

---
+++ src/config/app_config.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <Arduino.h>
+#include <cstdint>
+
+#define DEBUGGING true
+
+#if DEBUGGING == true
+    #define debug(x)    Serial.print(x)
+    #define debugln(x)  Serial.println(x)
+#else
+    #define debug(x)
+    #define debugln(x)
+#endif
+
+namespace cfg {
+    constexpr int LOOP_INTERVAL_MS = 10;
+    constexpr int LOOP_INTERVAL_TEST_SHORT = 100; 
+    constexpr int LOOP_INTERVAL_TEST_LONG = 1000;
+    constexpr uint32_t BTN_DEBOUNCE_MS = 50;
+    constexpr uint32_t BTN_SHORT_MS = 200;
+    constexpr uint32_t BTN_LONG_MS = 1000;
+}
+   
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,52 @@
 #include <Arduino.h>
 
 
+#include "app_config.h"
+
+/// @brief Stack sizes for tasks
+constexpr int LISTENER_STACK = 2048;
+constexpr int HANDELER_STACK = 4096;
+
+/// @brief Task priorities
+constexpr UBaseType_t LISTENER_PRIORITY = 1;
+constexpr UBaseType_t HANDLER_PRIORITY = 2;
+
+/// @brief Task handles
+TaskHandle_t listener_t = nullptr;
+TaskHandle_t handler_t = nullptr;
+
+/// @brief Setup function for Arduino framework
+void listenerTask(void *param);
+void handlerTask(void *param);
+
+
+
 void setup() {
   Serial.begin(115200);
-}
+
+  debugln("======Starup========");
+  configASSERT(xTaskCreatePinnedToCore(listenerTask,"ListenerTask",LISTENER_STACK,0,LISTENER_PRIORITY,&listener_t,0) == pdPASS);
+  delay(50); // Give some time for the listener task to start
+  configASSERT(xTaskCreatePinnedToCore(handlerTask,"HandlerTask",HANDELER_STACK,0,HANDLER_PRIORITY,&handler_t,1) == pdPASS);
+  delay(50); // Give some time for the handler task to start  
+  debugln("======Setup Done========");
+} 
 
 void loop() {
-  Serial.println("Hello, RTOS!");
+  vTaskDelete(nullptr); // Delete the loop task as it's not needed
+}
+void listenerTask(void *param) {
+
+  TickType_t lastWakeTime = xTaskGetTickCount();
+  for(;;){
+    debugln("Listener Task Running");
+    vTaskDelayUntil(&lastWakeTime, pdMS_TO_TICKS(cfg::LOOP_INTERVAL_TEST_SHORT));
+  }
+}
+void handlerTask(void *param) {
+  TickType_t lastWakeTime = xTaskGetTickCount();
+  for(;;){
+    debugln("Handler Task Running");
+    vTaskDelayUntil(&lastWakeTime, pdMS_TO_TICKS(cfg::LOOP_INTERVAL_TEST_SHORT));
+  }
 }
